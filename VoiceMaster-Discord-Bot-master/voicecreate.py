9ceb61d374d82a48c7235e1850a683a299ae627a
Dusreza <52199517+Dusreza@users.noreply.github.com>
2020-10-03 20:18:52 +0200

    Add files via upload

---
+++ VoiceMaster-Discord-Bot-master/voicecreate.py
@@ -0,0 +1,35 @@
+import discord
+import math
+import asyncio
+import aiohttp
+import json
+from discord.ext import commands
+from random import randint
+import traceback
+import sqlite3
+import sys
+
+intents = discord.Intents.default()
+intents.members = True  # Subscribe to the privileged members intent.
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+bot.remove_command("help")
+
+initial_extensions = ['cogs.voice']
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(DISCORD_TOKEN)
